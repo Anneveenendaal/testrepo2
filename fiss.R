c27cd75a2cf2be4e49a0b8c1f898d1083e4ce650
Anne van Veenendaal <a.vanveenendaal@tilburguniversity.edu>
2022-04-21 22:06:02 +0200

    added some code

--- fiss.R
+++ fiss.R
@@ -1,1 +1,10 @@
-#Hi there!
+library(tidyverse)
+library(palmerpenguins)
+
+install.packages("palmerpenguins")
+install.packages("rtools")
+library(palmerpenguins)
+
+penguins %>%
+  ggplot(aes(x = bill_depth_mm))
+  geom_histogram()
